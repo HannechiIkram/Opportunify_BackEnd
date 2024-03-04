a162c5eba9f9d725f4181e779a0adde7f31fff41
samar <samar.rebhi@esprit.tn>
2024-03-05 00:19:59 +0100

    controle_de_saisie_du_register_jobseeker

--- Controllers/User-jobseekerController.js
+++ Controllers/User-jobseekerController.js
@@ -6,20 +6,63 @@ const registerUserjobseeker = async (req,res) => {
   try {
     const { name,lastname,birthdate, email,phone,address, password,role_jobseeker } = req.body;
 
+// required sur tous les champs ils doivent etre remplis
     if (!name || !lastname||!birthdate||!email ||!phone||!address ||!password ||!role_jobseeker) {
       return res.status(400).json({ error: 'All the fields are required' });
     }
-/*//// controle de saisie à refaire apres celui du frontend (meme controle)
-    if (password.length < 6) {
-      return res.status(400).json({ error: 'Password must be at least 6 characters long' });
-    }
-*/
+//// controle de saisie à refaire apres celui du frontend (meme controle)
+
+//validation du mot de passe
+const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+
+if (!passwordRegex.test(password)) {
+    return res.status(400).json({ error: 'Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, and one digit.' });
+}
+//validation du name w lastname 
+const nameRegex = /^[A-Za-z]+$/;
+
+// Minimum and maximum allowed length for first name and last name
+const minNameLength = 2;
+const maxNameLength = 10;
+
+if (!nameRegex.test(name)) {
+    return res.status(400).json({ error: 'name must contain only alphabetic characters.' });
+}
+
+if (!nameRegex.test(lastname)) {
+    return res.status(400).json({ error: 'lastname must contain only alphabetic characters.' });
+}
+
+if (name.length < minNameLength || name.length > maxNameLength) {
+    return res.status(400).json({ error: `name must be between ${minNameLength} and ${maxNameLength} characters long.` });
+}
+
+if (lastname.length < minNameLength || lastname.length > maxNameLength) {
+    return res.status(400).json({ error: `name must be between ${minNameLength} and ${maxNameLength} characters long.` });
+}
+
+// validation mtaa el mail
+ // Regular expression for email format validation
+ const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+ if (!emailRegex.test(email)) {
+     return res.status(400).json({ error: 'Invalid email format.' });
+ }
+ // validation de l'adress
+ if (!address || address.length < 5) {
+  return res.status(400).json({ error: 'Address must be at least 5 characters long.' });
+}
+
+
+
 
 // Check if user already exists
 const existingUser = await UserModel.findOne({ email });
 if (existingUser) {
   return res.status(400).json({ error: 'Email is already taken' });
 }
+
+
 // crypter le mdp
     const hashedPassword = await hashPassword(password);
 
