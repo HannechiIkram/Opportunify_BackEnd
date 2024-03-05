1a23c2bbb1b78665fb020cbdf0c4eb0bb8179dd5
samar <samar.rebhi@esprit.tn>
2024-03-05 20:06:43 +0100

    fix_login

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
+const { registerUser}=require('../Controllers/UserController');
 const {registerUserjobseeker}=require('../Controllers/User-jobseekerController');
 
 router.post('/registeruser',registerUser)
