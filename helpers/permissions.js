0e76e00c87026f9cfde6529767a3b4944d4026ac
samar <samar.rebhi@esprit.tn>
2024-03-05 02:09:49 +0100

    permissionsV0

---
+++ helpers/permissions.js
@@ -0,0 +1,18 @@
+// permissions.js
+
+const rolePermissions = {
+    job_seeker: {
+      frontoffice: ['view_jobs', 'apply_jobs', 'update_profile']
+    },
+    company: {
+      frontoffice: ['post_jobs', 'view_applications', 'manage_profile']
+    },
+    admin: {
+      backoffice: ['dashboard', 'manage_users', 'manage_jobs']
+    }
+  };
+  
+  module.exports = {
+    rolePermissions
+  };
+  
--- models/user.js
+++ models/user.js
@@ -1,9 +1,23 @@
+
 const mongoose = require('mongoose');
+const { rolePermissions } = require('../helpers/permissions');
 
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true },
   password: { type: String, required: true },
-  role: { type: String, enum: ['job_seeker', 'company', 'admin']},
+  role: { type: String, enum: ['job_seeker', 'company', 'admin'], required: true },
+  permissions: {
+    type: Object,
+    default: {},
+    select: false 
+  }
+});
+
+// Add permissions based on user role
+userSchema.pre('save', function(next) {
+  const role = this.role;
+  this.permissions = rolePermissions[role] || {};
+  next();
 });
 
 module.exports = mongoose.model('User', userSchema);
