e9484ce4d1c4a71f14e8b9c65a1f9968f150c296
HannechiIkram <ikram.hannechi@esprit.tn>
2024-04-23 21:20:57 +0100

    mege

--- app.js
+++ app.js
@@ -1,31 +1,27 @@
-
+// Importations
 const createError = require('http-errors');
 const express = require('express');
 const logger = require('morgan');
 const accessControl = require('../Opportunify_BackEnd/midill/accescontrol');
-
-
 const bodyParser = require("body-parser");
-const mongoose = require("mongoose"); // Importez Mongoose ici
+const mongoose = require("mongoose");
 const mongoconnection = require("./database/mongodb.json");
 const cors = require('cors');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 require('dotenv').config();
+
+// Routes
 const indexRouter = require('./routes/index');
 const applicationRouter = require('./routes/application');
 const userRouter = require('./routes/users');
+const jobOfferRouter = require("./routes/job_offer");
+
 const app = express();
 
-
-// Connexion à la base de données MongoDB avec Mongoose
+// Connexion à MongoDB
 mongoose.connect(
-  mongoconnection.url, 
-  ///no need for these
-  /*{
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }*/
+  mongoconnection.url
 )
 .then(() => {
   console.log("Connected to DB");
@@ -33,41 +29,30 @@ mongoose.connect(
 .catch((err) => {
   console.log(err);
 });
+
+// Configuration des middlewares
 app.use(cors({
   origin: 'http://localhost:5173',
-  methods: ['GET', 'POST','PUT','DELETE'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true 
+  credentials: true,
 }));
-
-
-app.use(cors({
-  origin: 'http://localhost:5173',
-  methods: ['GET', 'POST','PUT','DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true 
-}));
-
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(cookieParser());
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }));
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Utilisation des routes
 app.use('/', indexRouter);
-const jobOfferRouter = require("./routes/job_offer");
 app.use("/job_offer", jobOfferRouter);
-
-//les middleware eli teb3in jsonwebtoken
 app.use('/applications', applicationRouter);
-app.use('/user',userRouter);
-app.use(cookieParser());
-app.use(express.urlencoded({extended:false}));
+app.use('/user', userRouter);
+
+// Gestion des erreurs
 app.use(function(req, res, next) {
   next(createError(404));
 });
